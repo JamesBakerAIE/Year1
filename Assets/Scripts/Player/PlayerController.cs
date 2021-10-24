4cca08ebcfbfdeabc13b5c0f5877969ca77d2b54
James Baker <s191482@students.aie.edu.au>
2021-10-25 10:16:18 +1030

    # Fixed Attack State namespace # Player controller - preparing for states + State machine class

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 namespace Player
 {
-
-    enum State
-    {
-        Walk,
-        Sprint,
-        Crouch,
-        Hide,
-        Puzzle,
-    }
     public class PlayerController : MonoBehaviour
     {
         [Header("Player Movement")]
@@ -20,11 +11,8 @@ namespace Player
         [SerializeField] private Animator playerAnimator = null;
 
         [HideInInspector] public Vector3 move = Vector3.zero;
-        [SerializeField] private float walkSpeed = 0f; 
+        [SerializeField] private float walkSpeed = 0f;
 
-
-
-        private State state;
 
         [Header("Player Looking")]
         [SerializeField] private Transform playerCamera = null;
@@ -34,54 +22,16 @@ namespace Player
 
         [Header("Player Misc.")]
         [SerializeField] private SphereCollider noiseCollider = null;
-
+        [SerializeField] private float noiseRadius = 0f;
 
         private void Start()
         {
-            state = State.Walk;
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
+
         }
 
         private void Update()
         {
-            CheckState();
+
         }
-
-        public void CheckState()
-        {
-            switch(state)
-            {
-                case State.Walk:
-                    Movement(walkSpeed);
-                    CameraLook();
-                    break;
-            }
-        }
-
-        private void Movement(float _speed)
-        {
-            Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-            inputDir.Normalize();
-
-            Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
-
-            controller.Move(velocity * Time.deltaTime);
-        }
-        private void CameraLook()
-        {
-            Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
-            cameraPitch -= mouseDelta.y * mouseSensitivity;
-            cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
-
-            playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-
-            transform.Rotate(Vector3.up * mouseDelta.x * mouseSensitivity);
-        }
-  
-
     }
-
 }
