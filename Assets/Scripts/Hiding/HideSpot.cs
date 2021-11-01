9b2f42cacb199bac2371ed283f3865e2880ca03c
James Baker <s191482@students.aie.edu.au>
2021-11-02 09:55:56 +1030

    Merge branch 'Greybox' of https://github.com/Team8AIE/Team8Repo into Greybox
    # Conflicts:
    #	Assets/Scenes/TestScene/TestScene2.unity
    #	Assets/Scripts/Player/HidingState.cs
    #	ProjectSettings/HDRPProjectSettings.asset

--- Assets/Scripts/Hiding/HideSpot.cs
+++ Assets/Scripts/Hiding/HideSpot.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class HideSpot : MonoBehaviour
 {
+    public GameObject front;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
