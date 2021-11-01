bcf3419d9dcd12214e4b6112cf12b39b5c0c81a7
Ethan Doyle <s191867@students.aie.edu.au>
2021-11-02 08:53:46 +1030

    + Modelled Hallways + Light Testing + Light Scripts

---
+++ Assets/Scripts/FlickerLightTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLightTest : MonoBehaviour
+{
+    public bool isFlickering = false;
+     public float timeDelay;
+
+    void Update()
+    {
+        if (isFlickering == false)
+        {
+            StartCoroutine(FlickeringLight());
+        }
+    }
+    IEnumerator FlickeringLight()
+    {
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+            timeDelay = Random.Range(0.01f, 0.3f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.01f, 0.3f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
