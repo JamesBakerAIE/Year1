c5435b6c03b351fa39ff4ea56262c99330ca9f75
Marshall_Scholz <s181125@students.aie.edu.au>
2021-11-02 10:40:17 +1030

    + Updated Enemy prefab

--- Assets/Scripts/Enemy/States/AgitatedState.cs
+++ Assets/Scripts/Enemy/States/AgitatedState.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Player;
 namespace EnemyAI
 {
     public class AgitatedState : State
     {
+        Vector3 playersLastPosition;
+        public override void Enter()
+        {
+            Debug.Log("Entered patrol state");
+            playersLastPosition = GameObject.FindObjectOfType<PlayerController>().transform.position;
+        }
         public override Vector3 LogicUpdate(Vector3 enemyPosition)
         {
-            return Vector3.zero;
+            return playersLastPosition;
         }
     }
 }
