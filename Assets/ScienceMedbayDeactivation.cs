3b1119c6b7733912e7d5bf220a1a900252d034c7
Ethan Doyle <s191867@students.aie.edu.au>
2021-11-09 10:12:35 +1030

    + Updated Lighting + Added lighting scripts + Added matiriels

---
+++ Assets/ScienceMedbayDeactivation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScienceMedbayDeactivation : MonoBehaviour
+{
+    public List<Light> lights = new List<Light>();
+
+    public ScienceMedbayLight[] scienceMedbayLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scienceMedbayLight = FindObjectsOfType<ScienceMedbayLight>();
+
+        foreach (var item in scienceMedbayLight)
+        {
+            lights.Add(item.GetComponent<Light>());
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (var item in lights)
+            {
+                item.gameObject.SetActive(!isActiveAndEnabled);
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/ScienceMedbayLights.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScienceMedbayLights : MonoBehaviour
+{
+    public GameObject lightOn;
+    public GameObject lightOff;
+
+    public List<Light> lights = new List<Light>();
+    public Light[] temp;
+
+    public ScienceMedbayLight[] scienceMedbayLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        //temp = FindObjectsOfType<Light>();
+
+        //foreach (var item in temp)
+        //{
+        //    lights.Add(item);
+        //}
+        //Debug.Log(lights);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
