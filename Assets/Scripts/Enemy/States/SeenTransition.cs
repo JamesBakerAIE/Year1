14526ab0f346abc347da893e47cc5ebffabb6cfd
Marshall Scholz <marshall.scholz@gmail.com>
2021-11-15 15:57:07 +1030

    * Cut AI SeenTransition code in half

--- Assets/Scripts/Enemy/States/SeenTransition.cs
+++ Assets/Scripts/Enemy/States/SeenTransition.cs
@@ -25,112 +25,30 @@ namespace EnemyAI
             //UPDATING ENTER AND EXIT FOR STATES AND TRANSITIONS
         }
 
+        State selectedState;
+        readonly List<Ray> rays;
         public override State CheckTransition(Vector3 enemyPositon, Vector3 playerPosition)
         {
 
             bool wasInFOV = parentTransition.inDirectFOV;
 
             Vector3 direction = (enemyPositon - playerPosition);
-            RaycastHit hit;
+            selectedState = null;
 
-            State selectedState = null;
-            bool hitPlayer = false;
             //an array that shoots in the player's direction
-            Ray middleRay = new Ray(enemyPositon + eyesOffset, -direction + directionOffset + new Vector3(0, 0, 1));
-            Ray leftRay = new Ray(enemyPositon + eyesOffset, -direction + directionOffset);
-            Ray rightRay = new Ray(enemyPositon + eyesOffset, -direction + directionOffset + new Vector3(0, 0, -1));
 
-            if (Physics.Raycast(middleRay, out hit, Mathf.Infinity, ~ignoreLayer))
-            {
-                //Player is in direct sight to the enemy
-                if (hit.collider.tag == "Player" && parentTransition.inFOV)
-                {
+            Vector3 rayStart = enemyPositon + eyesOffset;
+            Vector3 rayDestination = direction + directionOffset;
 
-                    //In attack range
-                    Debug.DrawRay(middleRay.origin, middleRay.direction * hit.distance, Color.green, 1f);
-                    if (parentTransition.inAttack)
-                    {
-                        Debug.Log("attack");
-                        parentTransition.inDirectAttack = true;
-                        parentTransition.inDirectFOV = true;
-                        hitPlayer = true;
-                        selectedState = parentTransition.attackState;
-                    }
-                    //In view range
-                    else if (parentTransition.inFOV)
-                    {
-                        parentTransition.inDirectFOV = true;
-                        selectedState = parentTransition.chaseState;
-                        hitPlayer = true;
-
-                    }
-
-                }
-
-            }
+            bool hitPlayer = false;
 
             if (hitPlayer == false)
-            {
-                if (Physics.Raycast(leftRay, out hit, Mathf.Infinity, ~ignoreLayer))
-                {
-                    //Player is in direct sight to the enemy
-                    if (hit.collider.tag == "Player" && parentTransition.inFOV)
-                    {
-
-                        //In attack range
-                        Debug.DrawRay(leftRay.origin, leftRay.direction * hit.distance, Color.green, 1f);
-                        if (parentTransition.inAttack)
-                        {
-                            Debug.Log("attack");
-                            parentTransition.inDirectAttack = true;
-                            parentTransition.inDirectFOV = true;
-                            selectedState = parentTransition.attackState;
-                            hitPlayer = true;
-                        }
-                        //In view range
-                        else if (parentTransition.inFOV)
-                        {
-                            parentTransition.inDirectFOV = true;
-                            selectedState = parentTransition.chaseState;
-                            hitPlayer = true;
-
-                        }
-
-                    }
-
-                }
-            }
+                hitPlayer = ShootRay(rayStart, rayDestination);
             if (hitPlayer == false)
-            {
-                if (Physics.Raycast(rightRay, out hit, Mathf.Infinity, ~ignoreLayer))
-                {
-                    //Player is in direct sight to the enemy
-                    if (hit.collider.tag == "Player" && parentTransition.inFOV)
-                    {
+                hitPlayer = ShootRay(rayStart, rayDestination + new Vector3(0, 0, 1));
+            if (hitPlayer == false)
+                hitPlayer = ShootRay(rayStart, rayDestination + new Vector3(0, 0, -1));
 
-                        //In attack range
-                        Debug.DrawRay(rightRay.origin, rightRay.direction * hit.distance, Color.green, 1f);
-                        if (parentTransition.inAttack)
-                        {
-                            Debug.Log("attack");
-                            parentTransition.inDirectAttack = true;
-                            parentTransition.inDirectFOV = true;
-                            selectedState = parentTransition.attackState;
-                            hitPlayer = true;
-
-                        }
-                        //In view range
-                        else if (parentTransition.inFOV)
-                        {
-                            parentTransition.inDirectFOV = true;
-                            selectedState = parentTransition.chaseState;
-                            hitPlayer = true;
-                        }
-
-                    }
-
-                }
-            }
             //Player isn;t in direct sight of the enemy
             if (hitPlayer == false)
             {
@@ -152,6 +70,42 @@ namespace EnemyAI
             wasInFOV = parentTransition.inDirectFOV;
 
             return selectedState;
+        }
+
+        bool ShootRay(Vector3 enemyPosition, Vector3 direction)
+        {
+            Ray ray = new Ray(enemyPosition, -direction);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~ignoreLayer))
+            {
+                //Player is in direct sight to the enemy
+                if (hit.collider.tag == "Player" && parentTransition.inFOV)
+                {
+
+                    //In attack range
+                    Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green, 1f);
+                    if (parentTransition.inAttack)
+                    {
+                        Debug.Log("attack");
+                        parentTransition.inDirectAttack = true;
+                        parentTransition.inDirectFOV = true;
+                        selectedState = parentTransition.attackState;
+                        return true;
+                    }
+                    //In view range
+                    else if (parentTransition.inFOV)
+                    {
+                        parentTransition.inDirectFOV = true;
+                        selectedState = parentTransition.chaseState;
+                        return true;
+
+                    }
+
+                }
+                //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 1f);
+            }
+            return false;
+
         }
 
     }
