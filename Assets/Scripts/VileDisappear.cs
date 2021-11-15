767a4bcab17200c633e5db57890b1707d7d3299f
Matthew Lynch <s191314@students.aie.edu.au>
2021-11-15 16:49:16 +1030

    VileDisappear (deletes object when you click it) Script, Electrical Spark Particle Effect re-done and finished. Lockers in place and working

---
+++ Assets/Scripts/VileDisappear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VileDisappear : MonoBehaviour
+{
+    public GameObject Vile;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+
+}
+
