7281bd3139144d08d6b9abc257615f56dadf7b82
James Baker <s191482@students.aie.edu.au>
2021-11-16 15:46:48 +1030

    + Started on gas timer

---
+++ Assets/Scripts/Timer/IncreaseGas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GasTimer
+{
+    public class IncreaseGas : MonoBehaviour
+    {
+
+        public float density = 0.0f;
+
+        private float timerDuration = 3f * 60f;
+        private float timer;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                //UpdateTimerDisplay(timer);
+            }
+        }
+
+        public void IncreaseDensity(float time)
+        {
+
+        }
+    }
+}
