fd1a29be8abe72972545e3452d86cbbe5e1e7f37
Ethan Doyle <s191867@students.aie.edu.au>
2021-11-23 12:08:46 +1030

    MISC changes

---
+++ Assets/Scripts/Puzzles/ComputerPuzzle/ComputerPuzzle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzle
+{
+    public class ComputerPuzzle : Puzzle
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
