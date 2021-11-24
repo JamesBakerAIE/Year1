dfba7476bfb003e2a2dde2d93deaf728e5e5310e
James Baker <s191482@students.aie.edu.au>
2021-11-24 12:13:59 +1030

    + Added box explosion to the main menu scene when the player clicks play

---
+++ Assets/Scripts/MainMenuEvent/MainMenuEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuEvent : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public Vector3 force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartEvent()
+    {
+        rigidbody.AddForce(force, ForceMode.Impulse);
+    }
+
+}
