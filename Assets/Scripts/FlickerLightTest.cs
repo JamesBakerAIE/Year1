0acf6b6b72cf99e12843783d0ef431659584f94a
Ethan Doyle <s191867@students.aie.edu.au>
2021-11-30 12:04:05 +1030

    * Lights flicker fixed

--- Assets/Scripts/FlickerLightTest.cs
+++ Assets/Scripts/FlickerLightTest.cs
@@ -5,27 +5,38 @@ using UnityEngine;
 public class FlickerLightTest : MonoBehaviour
 {
     public bool isflickering = false;
+    private Light light;
     public float timedelay;
 
-    private void ondisable()
+    private void OnDisable()
     {
     }
-    void update()
+    void Update()
     {
         if (isflickering == false)
         {
             StartCoroutine(flickeringlight());
         }
     }
-    IEnumerator flickeringlight()
+    public IEnumerator flickeringlight()
     {
-        isflickering = true;
-        this.gameObject.GetComponent<Light>().enabled = false;
-        timedelay = Random.Range(0.01f, 0.3f);
-        yield return new WaitForSeconds(timedelay);
-        this.gameObject.GetComponent<Light>().enabled = true;
-        timedelay = Random.Range(0.01f, 0.3f);
-        yield return new WaitForSeconds(timedelay);
-        isflickering = false;
+
+        yield return new WaitForSeconds(Random.Range(0.01f, 5f));
+        if(light == null)
+        {
+            light = gameObject.GetComponent<Light>();
+        }
+
+        light.enabled = !light.enabled;
+
+
+        //isflickering = true;
+        //this.gameObject.GetComponent<Light>().enabled = false;
+        //timedelay = Random.Range(0.01f, 0.3f);
+        //yield return new WaitForSeconds(timedelay);
+        //this.gameObject.GetComponent<Light>().enabled = true;
+        //timedelay = Random.Range(0.01f, 0.3f);
+        //yield return new WaitForSeconds(timedelay);
+        //isflickering = false;
     }
 }
