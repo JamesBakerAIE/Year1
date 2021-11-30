fe1c673382cac19688ecc0bf28fe2edcae3bc647
James Baker <s191482@students.aie.edu.au>
2021-11-30 15:00:56 +1030

    + Started on sound menu camera light thing

---
+++ Assets/Scripts/MainMenuEvent/MainMenuLightEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLightEvent : MonoBehaviour
+{
+    [SerializeField] private AudioSource breakSound;
+    [SerializeField] private AudioSource squeakSound;
+    [SerializeField] private AudioClip breakSoundClip;
+    [SerializeField] private GameObject soundArea;
+    [SerializeField] private Camera camera;
+
+    private bool isExecuting = false;
+    private bool readyToRotate = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+        //breakSoundClip = breakSound.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isExecuting == true && readyToRotate == true)
+        {
+            camera.transform.LookAt(soundArea.transform);
+        }
+    }
+
+    public void StartEvent()
+    {
+        isExecuting = true;
+
+        breakSound.Play();
+        StartCoroutine(WaitForBreakClipTime());
+        
+    }
+
+    public IEnumerator WaitForBreakClipTime()
+    {
+        yield return new WaitForSeconds(breakSoundClip.length);
+        readyToRotate = true;
+        squeakSound.Play();
+    }
+}
